cc1f2b980f64e2755e1e214054b667087d76d069
Anil khand <anilkhandthakuri@gmail.com>
2025-03-09 21:47:04 +1100

    Update

--- Documents/BTC-DentedCode/LMS-backend/lms-fe/src/pages/signin-singup/VerifyUser.jsx
+++ Documents/BTC-DentedCode/LMS-backend/lms-fe/src/pages/signin-singup/VerifyUser.jsx
@@ -6,6 +6,21 @@ import { toast } from "react-toastify";
 import { verifyUserAxios } from "../../features/user/userAxios";
 
 export const VerifyUser = () => {
+  const { _token } = useParams();
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleVerificaiton = async () => {
+    setIsLoading(true);
+    const { status, message } = await verifyUserAxios(_token);
+    setIsLoading(false);
+    if (status == "success") {
+      setSuccess(message);
+    } else {
+      setError(message);
+    }
+  };
 
   return (
     <div>
