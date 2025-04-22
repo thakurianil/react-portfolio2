eaf9aa7ca4dbd0b8ea02cb0866e0c8a0ae55f5d3
Anil khand <anilkhandthakuri@gmail.com>
2025-04-22 21:53:45 +1000

    Done with updates

--- Documents/BTC-DentedCode/LMS-backend/lms-fe/src/pages/signin-singup/Signup.jsx
+++ Documents/BTC-DentedCode/LMS-backend/lms-fe/src/pages/signin-singup/Signup.jsx
@@ -13,14 +13,6 @@ const SignUp = () => {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const handleOnChange = (e) => {
-    const { name, value } = e.target;
-
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
